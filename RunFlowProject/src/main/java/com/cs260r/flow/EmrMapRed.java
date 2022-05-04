6bcbdcd600d0fa8e6771227b678c43fe629a6bea
Priyan Vaithilingam <priyanmuthu@gmail.com>
2022-05-04 09:35:26 -0500

    incmomplete aws EMR mapreduce

--- RunFlowProject/src/main/java/com/cs260r/flow/EmrMapRed.java
+++ RunFlowProject/src/main/java/com/cs260r/flow/EmrMapRed.java
@@ -4,14 +4,25 @@ import java.io.IOException;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClientBuilder;
+import com.amazonaws.services.elasticmapreduce.model.StepConfig;
+import com.amazonaws.services.elasticmapreduce.util.StepFactory;
 
 public class EmrMapRed {
     public static void main(String[] args) {
+
+        // Initial code from: https://docs.aws.amazon.com/emr/latest/ManagementGuide/emr-common-programming-sample.html
+
         System.out.println("Hello!");
         AWSCredentials credentials_profile = null;
+        AWSCredentialsProvider credentials_provider = null;
         try{
-            credentials_profile = new ProfileCredentialsProvider("default").getCredentials();
+            credentials_provider = new ProfileCredentialsProvider("default");
+            credentials_profile = credentials_provider.getCredentials();
         } catch (Exception e){
             throw new AmazonClientException(
                     "Cannot load credentials from .aws/credentials file. " +
@@ -19,6 +30,17 @@ public class EmrMapRed {
                     e);
         }
 
-        // EMR class
+        // AWS EMR cluster
+        AmazonElasticMapReduce emr = AmazonElasticMapReduceClientBuilder.standard()
+                .withCredentials(credentials_provider)
+                .withRegion(Regions.US_WEST_1)
+                .build();
+
+        // Run a bash script using a predefined step in the StepFactory helper class
+        StepFactory stepFactory = new StepFactory();
+        StepConfig runBashScriptStep = new StepConfig()
+                .withName("Run Bash Script")
+                .withHadoopJarStep(stepFactory.newScriptRunnerStep("s3://jeffgoll/emr-scripts/create_users.sh"))
+                .withActionOnFailure("CONTINUE");
     }
 }
